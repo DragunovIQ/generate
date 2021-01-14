34dda7f4a8f0e354adea36d7a0ede751b40f2da3
Dragunov <smoke.hummer@hotmail.com>
2021-01-14 18:58:20 +0300

    update

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-var numbers = [0,37, 38, 32, 45, 21, 69, 20];
+var numbers = [16,26, 64, 88, 100, 23,7];
+var numbers_ig = [27,70, 132, 6, 30, 18,150];
 var counter = 0;
-function fetching(name){
+function fetching(name){ 
   return document.getElementById(name);
 }
-var text = "Mohammed Noah William Oliver Addilyn, Adley, Alisa, Alora, Analia, Aria, Armelle, Aviana William Elijah James Benjamin Lucas Mason Ethan Alexander Henry Jacob Michael Daniel Logan Jackson Sebastian Jack Aiden Owen Samuel Matthew Joseph Levi Mateo David John Wyatt Carter Julian";
-var new_arr = text.split(" ");
-var names = fetching("names");
-function generateNumber(index) {
-  var arr_rand = Math.floor(Math.random() * numbers.length);
-  var rand = Math.floor(Math.random() * 39);
-  var desired = rand;
-  var wanted = new_arr[desired];
-  var duration = 2000;
-  var output = $('#output' + index); // Start ID with letter
-  var started = new Date().getTime();
-    animationTimer = setInterval(function() {
-      if (output.text().trim() === desired || new Date().getTime() - started > duration) {
-        clearInterval(animationTimer); // Stop the loop
-        output.text(desired);
-      } else {
-      output.text(
-        '' +
-        Math.floor(Math.random() * 11) +
-        Math.floor(Math.random() * 11)
-      );
-    }
-  }, 100);
-  setTimeout(function(){alert(wanted);}, 2300);
-}
+var fb = "لميس_قاسم_المعموري ام_احمد Azhar_Jassim امجد_صباح Ali_AlKhafaji Aya_Aqeel_Asad Naz_Azad نور_المصطفى Sondos_Ali اميرة_المياحي زهرة_يوليو Harith_alaubaidi خلك_شايب زهراء_محمد_الموسوي حسين_علي_الملكي Wedad_Ali_Daham Alaa_aqeel نور_المصطفى مصطفى_نوري_الفيلي الفراشه_البيضاء فاتن_السعدي سوزان_مهدي_المندلاوية حنين_احمد فدك_حسين_وليد ميساء_يوسف جنة_الخلد_خلد مولاتي_فاطمه Shamoo_Hussen Noor_Alhuda_Noor_Alhuda مرتضى_علي Amar_Ali اراس_علي طه_مراد_علي دعاء_ﮩ'ۦ ريحانه_'ے مجيد_محمد_علي ابوحسن_السراجي Reema_Sabah محمد_المصطفى محمود_عبد_الله شهد_مهدي مرتض_العراقي علي_احمد Taha_Ahmed قاسم_محمد_الاعرجي Luay_Hasan سمر_قاسم ام_كيان حسين_حسين أبراهيم_الزهيري Mahmoud_Abo_Baqer الاء_محمد Sara_Ali Zachariah_David ام_علي هبة_خالد_المعموري سحر_الموسوي نورهان_علي Daila_Alaa_Albeity عازفة_الامل اسماء_ناظم ملاك_محمد Jore_Jore زينب_جاسم انسام_يوسف رياض_ابو_مصطفى Waled_Khalid Eliza_Beth Shvan_Stuni نرجس_قاسم_الاعرجي ام_ضياء عباس_قاسم عباس_قاسم_البديري Abbas_Al_Qasim حسوني_الفتلاوي رحيق_الزهور هہٰٰ۪۪۫زالہٰٰ۪۪۫_ﮧۦ_ﮧۦ وردة_الياسمين زهرة_الكاردينيا نسمات_هادئة Fa_Toshe عبودي_الاعرجي_الاعرجي لبنى_رحيم هدير_محمد حياه_ستار فراشه_الربيع الفراشة_البيضاء ام_احمد_الي_بروفايلها_صورة_اطفال_ثنين ياقوتة_ياقوتة Roraya_H_Ali أبراهيم_الزهيري Ahmad_AL_Rubaie كيك_ام_البنين يحيى_حسن Berwas_Aljaf مصطفى_نوري_الفيلي ميساء_يوسف املي_بالله حنين_احمد مصطفى_نوري_الفيلي جنة_الخلد_خلد محمود_عبد_الله Alsaaty_Ahm فدك_حسين_وليد Amar_Ali خطوات_ملكه Aliaa_Hussein Sondos_Ali Huda_Saleh ميساء_يوسف Umredaa_Alftlwi Lala_Queen Amar_Ali Alsaaty_Ahm";
+var insta = "reman1093 lilian._1998 sondos.alasadi nona49276 fkhmh292 amjed_alsahi99 lozz9.0 omnoo.r23 noor._.ali_99 om_entity2016 tqyt53410 zenobia._2019 rama._90_ hd00_80 i_tzrj nada9_12 reta_j.2014 afna_nmustafa nossa._.12 mysane.9 sarah.90.23 rwan200.1 to.to200.2 asea.aa roro81651 aamar909 r_f891 zahraa_420xx reemsaif38 fofo98951 __agjla shosha_qasim lll_mi90_ teeba3338 its_amir000 te8__a zahraa_420xx reemsaif38 fofo98951 __agjla shosha_qasim lll_mi90_ teeba3338 its_amir000 te8__a lilil1lili9 rwa2020n fatima_2020_2021 mhemn811 rodina_aa90 mhemn54 berwas_aljaf_97 lolo.lolo_19 mm_mo0 tota54634 aiq_.__ hydwry5359 zahr_a9h 2.0.0.o0 0lly_24 s_2000.t toto33569 764rfhj othmanm.8 elien._.94 jojo77083 lala_altememy mhhdy81 asso_5439 husseinkmh9 maloka.2211 narmeen.19pp w.d.r12 asaisha200 roro73828 minaduhoki10 omnya7158 trrlhw zain.ab5436 zozozozo901 husseinkmh9 maloka.2211 narmeen.19pp w.d.r12 asaisha200 roro73828 minaduhoki10 omnya7158 trrlhw zain.ab5436 zozozozo901 amnaabbas90 avb_exo noor_amlr kx2xi nhaqhfhta dygrhm aa__00_11 zozozozo901 amnaabbas90 avb_exo noor_amlr kx2xi nhaqhfhta dygrhm aa__00_11 txta.rt frel6632 sami4529_ rosem_ary1996 0_ba123 l_0fj tota_084 yassin_s7h laylav39 r_eham____ golden__rose3 mm_eay4 nour.427 ranha998 _b4sima8_ hanona_ahmed99 fy_8g25 ayat_1900 m4481hsn sm____sm_123 i.voq_ lanoo_._98 lolod_1643 alloush641 moh.eimn 1995.kdm 9jojo._.99 sahar_alfalahi82 meme_s127 0_fulaa om.saef86 ali_faisal_069 tallinn_98_ r__185r abbas1442qasim s5g_ meme_s127 0_fulaa om.saef86 ali_faisal_069 tallinn_98_ r__185r abbas1442qasim s5g_ rhaf.16";
+  var new_arr = insta.split(" ");
+  var names = fetching("names");
+  function generateNumber(index) { 
+    //var num_rand = Math.floor(Math.random() * numbers.length);
+    //var arr_rand = Math.floor(Math.random() * numbers.length);
+    var rand = Math.floor(Math.random() * new_arr.length);
+    var desired = numbers_ig[counter++];
+    var wanted = new_arr[desired]; 
+    var duration = 2000; 
+    var output = $('#output' + index); 
+    // Start ID with letter 
+    var started = new Date().getTime(); 
+    animationTimer = setInterval(function() { 
+      if (output.text().trim() === desired || new Date().getTime() - started > duration) { clearInterval(animationTimer); 
+      // Stop the loop 
+      output.text(desired); 
+    } else { 
+      output.text( '' + Math.floor(Math.random() * 11) + Math.floor(Math.random() * 11) ); 
+    } 
+    }, 100); setTimeout(function(){alert(wanted);}, 2300);
+  }
 var txt = ""
 var cc = 0;
 new_arr.forEach(value => txt = txt + cc++ + ": " + value + "<br>" );
